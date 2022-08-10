a2e981022c251befa8a625d45b661c86562bec76
Роман Бухмистов <rrb13@mail.ru>
2022-08-10 11:45:14 +0300

    Вторая домашняя работа по C#

---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+
+Console.WriteLine("Программа вывода третьей цифры");
+Console.WriteLine("Введите число: ");
+
+string? number = Console.ReadLine();
+int numberCheck = int.Parse(number);
+if (numberCheck > 99)
+{
+    Console.WriteLine(number[2]);
+}
+else if (numberCheck < -99)
+{
+    Console.WriteLine(number[3]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
---
+++ task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным
+Console.WriteLine("Программа проверки выходного дня");
+Console.WriteLine("Введите порядковый номер дня недели:");
+
+string? number = Console.ReadLine();
+int numberCheck = int.Parse(number);
+
+if (numberCheck > 5)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
